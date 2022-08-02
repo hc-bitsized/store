1c73c51209f4492adcacfb488185780490857685
danilo-1647822560 <danilolantunes@hotmail.com>
2022-08-01 23:58:09 -0300

    feat: add new route of health check

--- express/src/routes/healthcheck.js
+++ express/src/routes/healthcheck.js
@@ -1,4 +1,8 @@
 module.exports = function (app) {
+  app.get('/', function (_req, res) {
+    res.send({'api_status': 'ok'})
+  });
+
   app.get('/api', function (_req, res) {
     res.send({'api_status': 'ok'})
   });
